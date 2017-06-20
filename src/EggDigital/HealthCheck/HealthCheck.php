db3ffe0d783b78d56577e54b81593673033a09b1
Datenarong Tarin <datenarong@gmail.com>
2017-06-20 15:46:50 +0700

    [fix] Core : call classes

--- src/EggDigital/HealthCheck/HealthCheck.php
+++ src/EggDigital/HealthCheck/HealthCheck.php
@@ -14,63 +14,54 @@ class HealthCheck
         echo 'Class Name Does Not Exists';
     }
 
-    public static function output($datas)
+    public static function output($datas, $title = null)
     {
         $output = new \EggDigital\HealthCheck\Classes\Output;
-        return $output->html($datas);
+        return $output->html($datas, $title);
     }
 
     private static function cassandra($module_name)
     {
-        $cassandra = new \EggDigital\HealthCheck\Classes\Cassandra;
-        return $cassandra($module_name);
+        return new \EggDigital\HealthCheck\Classes\Cassandra($module_name);
     }
 
     private static function file($module_name)
     {
-        $file = new \EggDigital\HealthCheck\Classes\File;
-        return $file($module_name);
+        return new \EggDigital\HealthCheck\Classes\File($module_name);
     }
 
     private static function gearman($module_name)
     {
-        $gearman = new \EggDigital\HealthCheck\Classes\Gearman;
-        return $gearman($module_name);
+        return new \EggDigital\HealthCheck\Classes\Gearman($module_name);
     }
 
     private static function mongo($module_name)
     {
-        $mongo = new \EggDigital\HealthCheck\Classes\Mongo;
-        return $mongo($module_name);
+        return new \EggDigital\HealthCheck\Classes\Mongo($module_name);
     }
 
     private static function mysql($module_name)
     {
-        $mysql = new \EggDigital\HealthCheck\Classes\Mysql;
-        return $mysql($module_name);
+        return new \EggDigital\HealthCheck\Classes\Mysql($module_name);
     }
 
     private static function oracle($module_name)
     {
-        $oracle = new \EggDigital\HealthCheck\Classes\Oracle;
-        return $oracle($module_name);
+        return new \EggDigital\HealthCheck\Classes\Oracle($module_name);
     }
 
     private static function redis($module_name)
     {
-        $redis = new \EggDigital\HealthCheck\Classes\Redis;
-        return $redis($module_name);
+        return new \EggDigital\HealthCheck\Classes\Redis($module_name);
     }
 
     private static function socket($module_name)
     {
-        $socket = new \EggDigital\HealthCheck\Classes\Socket;
-        return $socket($module_name);
+        return new \EggDigital\HealthCheck\Classes\Socket($module_name);
     }
 
     private static function curl($module_name)
     {
-        $curl = new \EggDigital\HealthCheck\Classes\Api;
-        return $curl($module_name);
+        return new \EggDigital\HealthCheck\Classes\Api($module_name);
     }
 }
