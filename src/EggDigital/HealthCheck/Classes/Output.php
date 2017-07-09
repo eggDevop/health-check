675de1a772a54c7fee095f6d2dafbabcf934c8fe
gchatchaimee <chatchai.meesuksabai@gmail.com>
2017-07-09 21:46:41 +0700

    Fixed style display table

--- src/EggDigital/HealthCheck/Classes/Output.php
+++ src/EggDigital/HealthCheck/Classes/Output.php
@@ -38,14 +38,18 @@ class Output
                 50% { opacity: 0; }
             }
 
-           .circle {
+            .circle {
                 width: 20px;
                 height: 20px;
                 background: red;
                 -moz-border-radius: 50px;
                 -webkit-border-radius: 50px;
                 border-radius: 50px;
-           }
+            }
+
+            td {
+                white-space: nowrap;
+            }
             </style>
         ';
 
@@ -75,7 +79,6 @@ class Output
             <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-            <form>&nbsp;<input type=button value="Refresh" onClick="javascript:location.reload();"></form>
         ';
 
         return $footer;
